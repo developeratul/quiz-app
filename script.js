7d8813a5b83151fc0c3d5b5a62fd252a59ac39fe
Ratul <azammmgol@gmail.com>
2021-02-06 12:45:55 +0600

    all done

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const qustionDataBase = [
     question: 'Full form of Html?',
     option1: 'Hello to my world',
     option2: 'Hey text my language',
-    option3: 'Hyper text Merkup language',
+    option3: 'Hyper text Markup language',
     option4: 'Hyper text Makeup language',
-    answer: 'answer3',
+    ans: 'answer3',
   },
   {
     question: 'why do we use ReactJs?',
@@ -13,7 +13,7 @@ const qustionDataBase = [
     option2: 'For back-end',
     option3: 'For data-bases',
     option4: "It's nothing.",
-    answer: 'answer1',
+    ans: 'answer1',
   },
   {
     question: 'Which tag is incorrect?',
@@ -21,22 +21,63 @@ const qustionDataBase = [
     option2: '<html>',
     option3: '<abbr>',
     option4: 'All are correct',
-    answer: 'answer4',
+    ans: 'answer4',
   },
 ]
+
+// getting referrence
 const questionContainer = document.querySelector('h2')
 const option1 = document.querySelector('#option1')
 const option2 = document.querySelector('#option2')
 const option3 = document.querySelector('#option3')
 const option4 = document.querySelector('#option4')
 const submitButton = document.querySelector('button')
+const usersAnswer = document.querySelectorAll('.answer')
+const scoreArea = document.querySelector('#ShowScore')
+
+// This function is rendering all the texts
+
+let questionCount = 0
+let score = 0
 const mainFunc = () => {
-  const list = qustionDataBase[0]
+  const list = qustionDataBase[questionCount]
   questionContainer.innerText = list.question
   option1.innerText = list.option1
   option2.innerText = list.option2
   option3.innerText = list.option3
   option4.innerText = list.option4
 }
-submitButton.addEventListener('click', () => {})
+
 mainFunc()
+
+// this function is for answer checking
+
+const goCheckAnswer = () => {
+  let answers
+  usersAnswer.forEach((data) => {
+    if (data.checked) {
+      answers = data.id
+    }
+  })
+  return answers
+}
+
+submitButton.addEventListener('click', () => {
+  const checkAnswer = goCheckAnswer()
+  console.log(checkAnswer)
+
+  if (checkAnswer === qustionDataBase[questionCount].ans) {
+    score++
+  }
+  questionCount++
+  if (questionCount < qustionDataBase.length) {
+    mainFunc()
+  } else {
+    scoreArea.style.display = 'block'
+    scoreArea.innerHTML = `
+      <h3>Your score is ${score} / ${qustionDataBase.length}</h3>
+      <button class='btn' onclick='location.reload()'>Play Again</button>
+      `
+  }
+})
+console.log(qustionDataBase[questionCount].ans)
